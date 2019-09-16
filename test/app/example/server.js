59abe9fae8c431d24e5f8ed66560faf6516db2d5
pnhung177 <pnhung177@acegik.net>
2019-09-16 10:52:39 +0700

    Refactoring the source code

--- test/app/example/server.js
+++ test/app/example/server.js
@@ -4,7 +4,7 @@ var app = require("devebot").launchApplication({
   appRootPath: __dirname
 }, [], [{
   name: "devebot-co-redis",
-  path: require('path').join(__dirname, '../../index')
+  path: require('path').join(__dirname, '../../../index')
 }]);
 
 if (require.main === module) app.server.start();
